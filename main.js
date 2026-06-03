c0a4a3cbbe6a377a2d498b6812c87371e1561530
DarioM1992 <dario21992@gmail.com>
2026-06-03 17:45:48 +0200

    inseriti ineerHTML e un div con id che venga chiamato

--- main.js
+++ main.js
@@ -47,4 +47,14 @@ if (eta < 18 ) {
 // console.log("Prezzo base senza sconto applicato (0.21€):" + prezzoBase.toFixed(2) + "€");
 // console.log("Totale:" + totale.toFixed(2) + "€")
 
+
+
+//blocco innerHTML al posto dei console.log
+document.getElementById('risultato').innerHTML =`
+    <p>Km percorsi: ${km}</p>
+    <p>Età passeggero: ${eta}</p>
+    <p>Sconto applicato: ${sconto}</p>
+    <p>Prezzo base: ${prezzoBase.toFixed(2)}€</p>
+    <p>Totale : ${totale.toFixed(2)}€</p>
+  `;
 }
